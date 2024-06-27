8aaf1d53e6d9b02ba9f83f66f5fc1ef32e3acb7b
SatriaAlwan <satria.alwand4@gmail.com>
2024-06-27 09:01:25 +0700

    pembuatan customlist tahap proses input

--- app/src/main/java/com/example/reservasiapk/ui/profile/ProfileFragment.java
+++ app/src/main/java/com/example/reservasiapk/ui/profile/ProfileFragment.java
@@ -10,29 +10,15 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
-import com.example.reservasiapk.databinding.FragmentNotificationsBinding;
-import com.example.reservasiapk.ui.notifications.NotificationsViewModel;
+import com.example.reservasiapk.R;
+import com.example.reservasiapk.databinding.FragmentProfileBinding;
+import com.example.reservasiapk.ui.profile.ProfileViewModel;
 
 public class ProfileFragment extends Fragment {
 
-    private FragmentNotificationsBinding binding;
-
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        NotificationsViewModel notificationsViewModel =
-                new ViewModelProvider(this).get(NotificationsViewModel.class);
-
-        binding = FragmentNotificationsBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
-
-        final TextView textView = binding.textNotifications;
-        notificationsViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
-        return root;
+        return inflater.inflate(R.layout.fragment_profile, null);
     }
 
-    @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        binding = null;
-    }
 }
